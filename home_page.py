c3f26ea4ec7e06ef611c5a56143efc9399f5653b
flyngrifmac <sipspacesip@gmail.com>
2025-05-22 09:52:32 +1200

    pretty version of home title page

--- home_page.py
+++ home_page.py
@@ -1,6 +1,12 @@
 from tkinter import *
 from tkinter import ttk
 
+from pyfonts import load_google_font
+
+font = load_google_font("Hammersmith One")
+
+
+
 root = Tk()
 
 root.title("CapsU")
@@ -10,16 +16,16 @@ root.configure(bg = "#17139C")
 master_frame = Frame(root)
 master_frame.pack(fill = "x", )
 
-app_name_header = Label(master_frame, text = "CapsU", font = ("bogus", "100", "bold"), bg = "#17139C", fg = "#EFEFEF", pady = "20")
+app_name_header = Label(master_frame, text = "CapsU", font = ("Hammersmith One", "50"), bg = "#17139C", fg = "#EFEFEF", pady = "20")
 app_name_header.pack(side = "top")
 
 home_page_toolbar = Label(master_frame)
-home_page_toolbar.pack()
+home_page_toolbar.pack(side = "top")
 
-schedule_frame = Label(master_frame)
-schedule_frame.pack()
+schedule_frame = Frame(master_frame, background = "#DD3E3E")
+schedule_frame.pack(side = "top")
 
-
+#schedule_header = 
 
 
 root.mainloop()
