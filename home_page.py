f9636a0ac02f63a88a1abd42e0f32d0a02a49cb7
flyngrifmac <sipspacesip@gmail.com>
2025-05-22 09:08:05 +1200

    Base window functions set up

---
+++ home_page.py
@@ -0,0 +1,25 @@
+from tkinter import *
+from tkinter import ttk
+
+root = Tk()
+
+root.title("CapsU")
+root.geometry("370x630")
+root.configure(bg = "#17139C")
+
+master_frame = Frame(root)
+master_frame.pack(fill = "x", )
+
+app_name_header = Label(master_frame, text = "CapsU", font = ("bogus", "100", "bold"), bg = "#17139C", fg = "#EFEFEF", pady = "20")
+app_name_header.pack(side = "top")
+
+home_page_toolbar = Label(master_frame)
+home_page_toolbar.pack()
+
+schedule_frame = Label(master_frame)
+schedule_frame.pack()
+
+
+
+
+root.mainloop()
---
+++ med_list_page.py
@@ -0,0 +1,18 @@
+from tkinter import *
+from tkinter import ttk
+
+root = Tk()
+
+root.title("CapsU")
+root.geometry("370x630")
+root.configure(bg = "#17139C")
+
+master_frame = ttk.Frame(root)
+master_frame.grid(row = 0, column = 0, sticky = "news")
+
+
+
+
+
+
+root.mainloop()
