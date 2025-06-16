2a690957ad3e7d90a3e5a55b3aedf88e38e8f9a3
flyngrifmac <sipspacesip@gmail.com>
2025-06-16 14:15:30 +1200

    title labels not frames

--- notification_page.py
+++ notification_page.py
@@ -33,24 +33,20 @@ current_timeslot_frame = ctk.CTkFrame(notifications_body)
 current_timeslot_frame.pack(side = "top", fill = "x", padx = "5")
 
 current_timeslot_title = ctk.CTkLabel(current_timeslot_frame, text = "Current Medication(s)")
-current_timeslot_title.pack(side = "top", fill = "x", padx = "5", anchor = "w")
+current_timeslot_title.pack(side = "top", padx = "5", anchor = "w")
 
-current_timeslot = ctk.CTkFrame(current_timeslot_frame)
-current_timeslot.pack(side = "top", fill = "x", pady = "30")
-
-current_timeslot_time = ctk.CTkLabel(current_timeslot, text = "9:30am")
-current_timeslot_time.pack(side = "top", anchor = "w")
-
-current_timeslot_content = ctk.CTkLabel(current_timeslot, text = "- 2 x ISOTRETINOIN. Take with food and water.")
-current_timeslot_content.pack(side = "top", anchor = "w")
+func.info_box(current_timeslot_frame, "9:30 am", "- 2 x ISOTRETINOIN. Take with food and water.")
 
 #UPDATES
-update_items = LabelFrame(notifications_body, text = "Updates")
-update_items.pack(side = "top", fill = "x", padx = "5")
+update_items_frame = ctk.CTkFrame(notifications_body)
+update_items_frame.pack(side = "top", fill = "x", padx = "5")
 
-func.info_box(update_items, "Prescription", "- Your ISOTRETINOIN prescription can now be renewed.")
+update_items_title = ctk.CTkLabel(update_items_frame, text = "Updates")
+update_items_title.pack(side = "top", padx = "5", anchor = "w")
 
-func.info_box(update_items, "Prescription", "- Your ADDERALL prescription can now be renewed.")
+func.info_box(update_items_frame, "Prescription", "- Your ISOTRETINOIN prescription can now be renewed.")
+
+func.info_box(update_items_frame, "Prescription", "- Your ADDERALL prescription can now be renewed.")
 
 
 #FOOTER
