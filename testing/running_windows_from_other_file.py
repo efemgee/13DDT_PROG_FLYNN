619548ecd2178559ae4cbdbff90921ba743b3bdf
flyngrifmac <sipspacesip@gmail.com>
2025-07-18 14:59:53 +1200

    failure of subprocessing tests and moving on to lifting

--- testing/running_windows_from_other_file.py
+++ testing/running_windows_from_other_file.py
@@ -1,3 +1,3 @@
 import subprocess
 
- subprocess.run(["python3", "/ subprocess.run(["python3", "different_windows_test.py"])
+subprocess.run(["python3", "C:\\Users\\sipsp\\CODING\\13DDT\\13DDT_PROG_FLYNN\\testing\\different_windows_test.py"])
