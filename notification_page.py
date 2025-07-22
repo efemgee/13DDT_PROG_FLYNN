c36b48eaed4f43de84b19987a748ce5ad3101fdc
flyngrifmac <sipspacesip@gmail.com>
2025-07-22 12:20:13 +1200

    formatting stuff

--- notification_page.py
+++ notification_page.py
@@ -8,28 +8,30 @@ root = ctk.CTk()
 
 root.title("CapsU")
 root.geometry("400x600")
+root.minsize(400, 600)
+root.maxsize(400, 600)
 
-master_frame = ctk.CTkFrame(root)
-master_frame.pack(fill = "both")
+master_frame = ctk.CTkFrame(root, width = 400, height = 600)
+master_frame.pack(side = "top", fill = "both")
 
 
 #HEADER
-notifications_frame = ctk.CTkFrame(master_frame)
-notifications_frame.pack(side = "top")
+notif_frame = ctk.CTkFrame(master_frame, width = 400, height = 00)
+notif_frame.pack(side = "top", fill = "both")
 
-schedule_header = ctk.CTkFrame(notifications_frame)
+schedule_header = ctk.CTkFrame(notif_frame)
 schedule_header.pack(side = "top")
 
 
 #NOTIFICATIONS BODY
-notifications_header_title = ctk.CTkLabel(schedule_header, text = "Notifications", font = ("Arial", 20))
-notifications_header_title.pack(side = "left", anchor = "w", fill = "x", expand = "True", pady = "15")
+notif_header_title = ctk.CTkLabel(schedule_header, text = "Notifications", font = ("Arial", 20))
+notif_header_title.pack(side = "left", anchor = "w", fill = "x", expand = "True", pady = "15")
 
-notifications_body = ctk.CTkFrame(notifications_frame)
-notifications_body.pack(side = "top")
+notif_body = ctk.CTkFrame(notif_frame)
+notif_body.pack(side = "top")
 
 #CURRENT MEDICATION TIMESLOT
-current_timeslot_frame = ctk.CTkFrame(notifications_body)
+current_timeslot_frame = ctk.CTkFrame(notif_body)
 current_timeslot_frame.pack(side = "top", fill = "x", padx = "5")
 
 current_timeslot_title = ctk.CTkLabel(current_timeslot_frame, text = "Current Medication(s)")
@@ -38,7 +40,7 @@ current_timeslot_title.pack(side = "top", padx = "5", anchor = "w")
 func.info_box(current_timeslot_frame, "9:30 am", "- 2 x ISOTRETINOIN. Take with food and water.")
 
 #UPDATES
-update_items_frame = ctk.CTkFrame(notifications_body)
+update_items_frame = ctk.CTkFrame(notif_body)
 update_items_frame.pack(side = "top", fill = "x", padx = "5")
 
 update_items_title = ctk.CTkLabel(update_items_frame, text = "Updates")
@@ -46,7 +48,7 @@ update_items_title.pack(side = "top", padx = "5", anchor = "w")
 
 func.info_box(update_items_frame, "Prescription", "- Your ISOTRETINOIN prescription can now be renewed.")
 
-func.info_box(update_items_frame, "Prescription", "- Your ADDERALL prescription can now be renewedhhhhhhhhhhhhhhhhhh.")
+func.info_box(update_items_frame, "Prescription", "- Your ADDERALL prescription can now be renewed.")
 
 
 #FOOTER
