64832a42e48130d70f6facc34f1fbd35d397693d
flyngrifmac <sipspacesip@gmail.com>
2025-08-05 11:38:46 +1200

    pages open

--- functions.py
+++ functions.py
@@ -1,7 +1,7 @@
 from tkinter import *
 from tkinter import ttk
 import customtkinter as ctk
-
+import pages
 
 
 #Function to create an approximate Tkinter LabelFrame in CTkinter.
@@ -35,21 +35,21 @@ def info_box(parent_frame, title = str, content = str):
     box_content.insert("0.0", content)
     box_content.pack(side = "top", anchor = "w")
 
-def footer_button(parent_frame, title = str):
-    button = ctk.CTkButton(parent_frame, text = title, width = 50)
+def footer_button(parent_frame, command_function, title = str):
+    button = ctk.CTkButton(parent_frame, text = title, command = command_function, width = 50)
     button.pack(side = "left", padx = 3)
 
 def footer(parent_frame):
     footer_frame = ctk.CTkFrame(parent_frame)
     footer_frame.pack(side = "bottom", pady = (20, 0))
 
-    footer_button(footer_frame, "Home")
+    #footer_button(footer_frame, "Home")
 
-    footer_button(footer_frame, "Calendar")
+    #footer_button(footer_frame, "Calendar")
 
-    footer_button(footer_frame, "Medications")
+    footer_button(footer_frame, pages.med_list_page, "Medications")
 
-    footer_button(footer_frame, "Notifications")
+    footer_button(footer_frame, pages.notification_page, "Notifications")
     
     
 def input_box(parent, title, placeholder):
@@ -59,4 +59,4 @@ def input_box(parent, title, placeholder):
     input_box_title.pack(side = "top")
     input_field = ctk.entry = ctk.CTkEntry(input_box_frame, placeholder_text=placeholder, width=140, height=20)
     input_field.pack(side = "top")
-    
+
