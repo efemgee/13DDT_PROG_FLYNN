e4e4179202b1517102c48266b76fd0412cdbab77
flyngrifmac <sipspacesip@gmail.com>
2025-08-31 19:52:55 +1200

    database creation thinking

--- testing/login_test_samples.py
+++ testing/login_test_samples.py
@@ -5,7 +5,7 @@ conn = sqlite3.connect("userdata.db")
 cur = conn.cursor()
 
 cur.execute("""
-CREATE TABLE IF NOT EXISTS userdata (
+CREATE TABLE IF NOT EXISTS logindata (
     id INTEGER PRIMARY KEY,
     username VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL
@@ -15,7 +15,17 @@ CREATE TABLE IF NOT EXISTS userdata (
 
 def new_user(username = str, password = str):
     username_to_enter, password_to_enter = username, hashlib.sha256(password.encode()).hexdigest()
-    cur.execute("INSERT INTO userdata (username, password) VALUES (?, ?)", (username_to_enter, password_to_enter))
+    cur.execute("INSERT INTO logindata (username, password) VALUES (?, ?)", (username_to_enter, password_to_enter))
+    new_user_id = cur.execute("""
+    SELECT id FROM logindata""")
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS AUTOINCREMENT_prescriptions (
+        id INTEGER PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        dose INTEGER NOT NULL,
+        time VARCHAR(255) NOT NULL
+    )
+    """)
 
 # username1, password1 = "frankinside", hashlib.sha256("bolts".encode()).hexdigest()
 # username2, password2 = "orpo", hashlib.sha256("poor".encode()).hexdigest()
